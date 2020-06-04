7a2ebc52e6d0b6170b77f1858a75cc3a66137b3a
杨林 <yanglin23@midea.com>
2020-06-04 13:56:38 +0800

    Run the whole algorithm process

--- toppra_test1/toppra_test1/main.cpp
+++ toppra_test1/toppra_test1/main.cpp
@@ -1,4 +1,7 @@
 #include "scara_joint_torque.h"
+#include "piecewise_poly_path.hpp"
+#include "qpOASES-wrapper.hpp"
+#include "toppra_alg.hpp"
 using namespace toppra;
 using namespace constraint;
 int main()
@@ -21,7 +24,36 @@ int main()
 
 	scara_joint_torque scara_trq(scara_lowerTlimit, scara_uperTlimit, scara_frictionCoeffs);
 
-	//std::cout << vd1<< endl;
+	toppra::Matrix coeff{ 4, 2 };
+	coeff(0, 0) = 0;
+	coeff(0, 1) = 0;
+	coeff(1, 0) = 1;
+	coeff(1, 1) = 1;
+	coeff(2, 0) = 2;
+	coeff(2, 1) = 2;
+	coeff(3, 0) = 3;
+	coeff(3, 1) = 3;
+	toppra::Matrices coefficents = { coeff, coeff };
+
+	std::shared_ptr<toppra::PiecewisePolyPath> path;
+	path = std::make_shared<toppra::PiecewisePolyPath>(coefficents, std::vector<double>{0, 1, 2});
+ 	//toppra::PiecewisePolyPath path =
+		//toppra::PiecewisePolyPath(coefficents, std::vector<double>{0, 1, 2});
+
+	toppra::LinearConstraintPtrs v;
+
+	v = toppra::LinearConstraintPtrs{ std::make_shared<toppra::constraint::scara_joint_torque>(scara_lowerTlimit, scara_uperTlimit, scara_frictionCoeffs) };
+
+	algorithm::TOPPRA ta(v, path);
+
+	std::shared_ptr<toppra::solver::qpOASESWrapper> qp_solver(new toppra::solver::qpOASESWrapper);
+
+	ta.solver(qp_solver);
+	ta.setN(50);
+	ta.computePathParametrization();
+
+	
+
 
 	return 0;
 }
